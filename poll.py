db29778d4c8731e8bdaf78ddd4235b2f1cc892e7
samiframadhan <sfauzanramadhan@gmail.com>
2024-11-19 20:54:14 +0700

    [Update]

--- poll.py
+++ poll.py
@@ -104,7 +104,7 @@ def process_data(gga_queue: Queue, data_queue: Queue, stop: Event):
                     logger.info(f"Alt: {parsed.alt}")
                 if hasattr(parsed, "quality"):
                     logger.info(f"Fix type: {parsed.quality}")
-                if hasattr(parsed, "text"):
+                if parsed.msgID == "TXT":
                     logger.info(f"TXT: {parsed.text}")
                 if parsed.msgID == "GGA":
                     gga_queue.put((raw_data, parsed))
