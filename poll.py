3263b9fd8f7cce8c5b09ade93a667e9ca037491e
samiframadhan <sfauzanramadhan@gmail.com>
2024-11-21 22:51:04 +0700

    [Update]

--- poll.py
+++ poll.py
@@ -174,6 +174,7 @@ def broadcast(tcp_server: TCPServer, gps_data_queue: Queue, ntrip_client: GNSSNT
             message = f"{lat.pop()},{long.pop()},{height.pop()},{fixtype},{PDOP.pop()},{HDOP.pop()},{VDOP.pop()},{connect}"
             logger.info(f"Broadcasting to tcp clients: {message}")
             tcp_server.broadcast(message=message)
+            gps_data_queue.task_done()
             
 
 def main(**kwargs):
