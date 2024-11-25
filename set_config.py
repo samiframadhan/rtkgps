44e368616f4b3f217f1eb2cd3c2660da3f926a5e
samiframadhan <sfauzanramadhan@gmail.com>
2024-11-25 08:42:17 +0700

    [Update]

--- set_config.py
+++ set_config.py
@@ -173,7 +173,7 @@ def main(**kwargs):
                 data_list = [
                         # ("CFG_NAVHPG_DGNSSMODE",""),
                         # ("CFG_NAVSPG_DYNMODEL",""),
-                        ("CFG_RATE_MEAS",150),
+                        ("CFG_RATE_MEAS",100),
                         ("CFG_RATE_NAV",1),
                         # ("CFG_RTCM_DF003_IN",""),
                         # ("CFG_RTCM_DF003_IN_FILTER",""),
