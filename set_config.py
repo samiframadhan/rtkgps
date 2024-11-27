64ff9889fb6086793d94e25f27e25826485b1797
samiframadhan <sfauzanramadhan@gmail.com>
2024-11-27 18:54:18 +0700

    [Update]

--- set_config.py
+++ set_config.py
@@ -180,7 +180,7 @@ def main(**kwargs):
                 data_list.append(("CFG_MSGOUT_UBX_NAV_HPPOSLLH_USB", 1))
                 data_list.append(("CFG_MSGOUT_UBX_NAV_POSLLH_USB", 1))
                 data_list.append(("CFG_MSGOUT_UBX_NAV_STATUS_USB", 1))
-                data_list.append(("CFG_RATE_MEAS", 400))
+                data_list.append(("CFG_RATE_MEAS", 600))
                 data_list.append(("CFG_RATE_NAV", 3))
                 
                 msg = UBXMessage.config_set(layer, transaction=0, cfgData=data_list)
