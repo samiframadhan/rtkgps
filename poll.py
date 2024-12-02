f96a3231cffce2173d83f5321c50786dd49165a5
samiframadhan <sfauzanramadhan@gmail.com>
2024-12-02 18:52:42 +0700

    [Update]

--- poll.py
+++ poll.py
@@ -222,6 +222,8 @@ def broadcast(tcp_server: TCPServer, gps_data_queue: Queue, ntrip_client: GNSSNT
 
             if count == 0:
                 height_m = height.pop() / 1000
+                long_data = long.pop()
+                lat_data = lat.pop()
                     
                 type = fix.pop()
                 if type == 1:
@@ -236,7 +238,7 @@ def broadcast(tcp_server: TCPServer, gps_data_queue: Queue, ntrip_client: GNSSNT
                     fixtype = "Float"
                 else:
                     fixtype = str(fix)
-                message = f"{lat.pop()},{long.pop()},{height_m:.4f},{fixtype},{numSV.pop()},{PDOP.pop()},{HDOP.pop()},{VDOP.pop()},{heading.pop()},{connect}" + "\r\n"
+                message = f"{lat_data:.9f},{long_data:.9f},{height_m:.4f},{fixtype},{numSV.pop()},{PDOP.pop()},{HDOP.pop()},{VDOP.pop()},{heading.pop()},{connect}" + "\r\n"
                 last_data = message
                 logger.info(f"Broadcasting to tcp clients: {last_data}")
                 tcp_server.broadcast(message=last_data)
